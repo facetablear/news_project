1fec39b7edbc3da41eb5b5ca945fe9f3bff0116d
facetablear <facetablear@gmail.com>
2024-09-20 11:21:50 +0300

    Добавил главную и тестовую страницу

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def test(request):
+    return HttpResponse("<h1>Тестовая страница с разметкой</h1>")
