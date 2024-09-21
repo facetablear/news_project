97827a3b8b8643155ef3c3a9f4acb1a959c21f61
facetablear <facetablear@gmail.com>
2024-09-21 12:35:07 +0300

    Константы для работы с изображениями

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Директория для сохранения изображений
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
