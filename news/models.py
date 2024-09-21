69cc31db3dba12469bff5e003c817040ae601301
facetablear <facetablear@gmail.com>
2024-09-21 11:40:33 +0300

    Создал модель новостей

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+"""Модель новостей"""
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    is_published = models.BooleanField(default=False)
+
