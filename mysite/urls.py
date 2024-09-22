ab050f62bf7dce4be7d3dc4060b43406125ff7da
facetablear <facetablear@gmail.com>
2024-09-22 17:39:54 +0300

    Изменил маршурт к главной странице

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
+    path('', include('news.urls')),
 ]
 
 # Подгрузка изображений при отладочном режиме
