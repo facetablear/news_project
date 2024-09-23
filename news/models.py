e3d540cdbe43c24bd5fd02fb25abcf593ccc9ddb
facetablear <facetablear@gmail.com>
2024-09-23 13:36:12 +0300

    Настройка модели новостей для админки

--- news/models.py
+++ news/models.py
@@ -4,13 +4,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего обновления')
+    image = models.ImageField(upload_to='images/%Y/%m/%d/', verbose_name='Изображение', blank=True)
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
